bb368e4833964dbd2394429fd8211bdb3a046c3f
TORBI <md.torbi@gmail.com>
2026-06-07 13:04:28 +0100

    Fix model load

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import numpy as np
 import joblib
 from datetime import datetime
 from model import TemporalFeatureExtractor
-import sys
-
-sys.modules['__main__'].TemporalFeatureExtractor = TemporalFeatureExtractor
+import model as model_module
+model_module.TemporalFeatureExtractor = TemporalFeatureExtractor
 
 # Configuration de la page
 st.set_page_config(
